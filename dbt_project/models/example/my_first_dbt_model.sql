3fade6cd5389e026e5bf7657de53e6fb0c6b27dd
Abduvakhid A <88376921+abdukz@users.noreply.github.com>
2022-03-22 04:54:10 -0500

    Update my_first_dbt_model.sql

--- dbt_project/models/example/my_first_dbt_model.sql
+++ dbt_project/models/example/my_first_dbt_model.sql
@@ -1,29 +1,30 @@
 WITH mdl AS (
             SELECT cn.t_date,
-                DATE_PART('year', cn.t_date) AS year,
-                cn.ticker, 
-                cn.weight, 
-                e.e_score, 
-                s.q_ending_total_return 
+                   DATE_PART('year', cn.t_date) AS year,
+                   cn.ticker, 
+                   cn.weight, 
+                   e.e_score, 
+                   s.q_ending_total_return 
             
-            FROM {{ source("public", "constituents") }} cn
-                LEFT JOIN {{source("public","escores")}} e ON e.t_date = cn.t_date AND e.ticker= cn.ticker
-                LEFT JOIN {{source("public","performance")}} s ON s.t_date = cn.t_date AND s.ticker = cn.ticker 
+            FROM  {{ source("public", "constituents") }} cn
+                  LEFT JOIN {{source("public","escores")}} e ON e.t_date = cn.t_date AND e.ticker= cn.ticker
+                  LEFT JOIN {{source("public","performance")}} s ON s.t_date = cn.t_date AND s.ticker = cn.ticker 
             )
 
-SELECT ticker, 
+
+SELECT     ticker, 
 	   t_date,
  	   e_score,
-       AVG(e_score) OVER W AS rolling_average,
+           AVG(e_score) OVER W AS rolling_average,
  	   COALESCE((STDDEV(e_score) OVER W),0) AS standard_deviation     
 
 FROM mdl
 
 WINDOW W AS (
-		  	PARTITION BY ticker 
- 		  	ORDER BY t_date ASC 
- 		  	RANGE BETWEEN 
- 		  	'1 year' PRECEDING 
- 		  	AND CURRENT ROW 
- 	  		)
+	    PARTITION BY ticker 
+ 	    ORDER BY t_date ASC 
+ 	    RANGE BETWEEN 
+ 	    '1 year' PRECEDING 
+ 	    AND CURRENT ROW 
+ 	     )
 
